b2ebf20305f2277fdf61bb79afe3dea6c6eacfcd
Emill <asasindark22@gmail.com>
2024-12-10 00:38:01 +0200

    creating a method for moving shapes

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,17 @@ namespace WinFormsApp1
         public int y;
         public abstract void DrawBkack(Graphics g);
         public abstract void HideDrawingBackGround(Graphics g);
+        public void MoveRight()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                HideDrawingBackGround(null);
+                x += 10;
+                DrawBkack(null);
+                System.Threading.Thread.Sleep(100);
+            }
+
+        }
 
 
 
